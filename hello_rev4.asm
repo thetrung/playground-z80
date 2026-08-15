7d25cbe891982ea61baf2a6d4accc7e8db2df444
Jack Dragoon <deulamco@gmail.com>
2026-08-15 20:19:53 +0700

    refine hello_rev4: LD SP,0xFFFF -> 0x0000

--- hello_rev4.asm
+++ hello_rev4.asm
@@ -1,10 +1,10 @@
-PORT_DISPLAY: EQU 00h       ; Changed 0x00 to 00h
+PORT_DISPLAY: EQU 0x00       ; Changed 0x00 to 00h
 
 ; Z80 Hello World via I/O Port 0x00
-ORG 0000h                   ; Changed 0x0000 to 0000h
+ORG 0x0000                   ; Changed 0x0000 to 0000h
 
 INIT:
-    LD SP, 0FFFFh           ; Changed 0xFFFF to 0FFFFh
+    LD SP, 0x0000           ; 0x0000 - 1 = 0xFFFF
     LD HL, MSG              ; Load the address of the string into HL register
 
 MAIN:
@@ -21,4 +21,4 @@ HALT_LOOP:
     JR HALT_LOOP            ; Safety loop in case of unexpected interrupts
 
 MSG:
-    DB "Hello World!", 0Ah, 0  ; Text string with LF (0Ah) and Null terminator
+    DB "Hello World!", 0xA, 0x0  ; Text string with LF (0Ah) and Null terminator
